91e50f3e5fcbcb804951a40cf77fe81c94ab3abb
Joel Jebitto <joeljebitto@gmail.com>
2025-05-25 23:42:32 +0530

    added 'add' REST API

--- src/main/java/com/joeljebitto/employeeCRUD/rest/EmployeeRestController.java
+++ src/main/java/com/joeljebitto/employeeCRUD/rest/EmployeeRestController.java
@@ -3,6 +3,9 @@ package com.joeljebitto.employeeCRUD.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,21 @@ public class EmployeeRestController {
   @GetMapping("/employees")
   public List<Employee> getAllEmployees() {
     return employeeService.getAllEmployees();
+  }
+
+  @GetMapping("/employees/{employeesId}")
+  public Employee getEmployee(@PathVariable int employeesId) {
+    Employee theEmployee = employeeService.getEmployee(employeesId);
+    if (theEmployee == null) {
+      throw new RuntimeException("Employee Id Not Found - " + employeesId);
+    }
+    return theEmployee;
+  }
+
+  @PostMapping("/employees")
+  public void addEmployee(@RequestBody Employee theEmployee) {
+    theEmployee.setId(0);
+    employeeService.saveEmployee(theEmployee);
+
   }
 }
