c8c78474089b7bb1b78e7a534b0366618b46e03f
Joel Jebitto <joeljebitto@gmail.com>
2025-05-26 09:57:08 +0530

    added Update Student RestApi

--- src/main/java/com/joeljebitto/employeeCRUD/rest/EmployeeRestController.java
+++ src/main/java/com/joeljebitto/employeeCRUD/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,11 @@ public class EmployeeRestController {
   public void addEmployee(@RequestBody Employee theEmployee) {
     theEmployee.setId(0);
     employeeService.saveEmployee(theEmployee);
+  }
 
+  @PutMapping("/employees")
+  public String updateEmployee(@RequestBody Employee theEmployee) {
+    employeeService.saveEmployee(theEmployee);
+    return "Done";
   }
 }
