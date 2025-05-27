cc1f374a0408f17f1af59ca799bc08897a845f91
Joel Jebitto <joeljebitto@gmail.com>
2025-05-27 08:36:43 +0530

    springdoc added

--- src/main/java/com/joeljebitto/employeeCRUD/dao/EmployeeRepository.java
+++ src/main/java/com/joeljebitto/employeeCRUD/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.joeljebitto.employeeCRUD.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.joeljebitto.employeeCRUD.entity.Employee;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
